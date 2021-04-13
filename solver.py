315498e6267a29d4cc0d0f4b3089d1e22f660d7e
kegacyunited <sahil.universe@gmail.com>
2021-04-12 23:50:13 -0400

    converted solver.py code to python class and function

--- solver.py
+++ solver.py
@@ -1,20 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-# address for the cryptogram solver website
-url = "http://rumkin.com/tools/cipher/cryptogram-solver.php"
+sample = 'DL KBZ PVVM ZBDE LKBL EVXFROBRT DZ LKV CFOZL QFOZL QFOX FQ WFNVOMXVML VJRVGL BUU LKV FLKVOZ LKBL KBNV PVVM LODVE.'
 
-# posting data to website
-r = requests.post(url, data = {
-    'dict': 'american-english-huge',
-    'text': 'DL KBZ PVVM ZBDE LKBL EVXFROBRT DZ LKV CFOZL QFOZL QFOX FQ WFNVOMXVML VJRVGL BUU LKV FLKVOZ LKBL KBNV PVVM LODVE.'
-}).text
+class solve_cryptogram:
+    def solve(self, cryptogram):
 
-# using bs4 (beautifulsoup) to parse reponses
+        # address for the cryptogram solver website
+        url = "http://rumkin.com/tools/cipher/cryptogram-solver.php"
 
-soup = BeautifulSoup(r, 'html.parser')
-i = 0
-for each in soup.findAll('div'):
-    if i == 6:
-        print(each.text)
-    i += 1
+        # posting data to website
+        r = requests.post(url, data = {
+            'dict': 'american-english-huge',
+            'text': cryptogram
+        }).text
+
+        # using bs4 (beautifulsoup) to parse reponses
+
+        soup = BeautifulSoup(r, 'html.parser')
+        i = 0
+        for each in soup.findAll('div'):
+            if i == 6:
+                return each.text
+            i += 1
+
+if __name__ == "__main__":
+    test = solve_cryptogram()
+    test.solve(sample)
