2409e00fd93555ba0566a23bdf5bb701adea3cf2
kegacyunited <sahil.universe@gmail.com>
2021-04-12 23:11:02 -0400

    added comments to solver.py

--- solver.py
+++ solver.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
+# address for the cryptogram solver website
 url = "http://rumkin.com/tools/cipher/cryptogram-solver.php"
 
+# posting data to website
 r = requests.post(url, data = {
     'dict': 'american-english-huge',
     'text': 'DL KBZ PVVM ZBDE LKBL EVXFROBRT DZ LKV CFOZL QFOZL QFOX FQ WFNVOMXVML VJRVGL BUU LKV FLKVOZ LKBL KBNV PVVM LODVE.'
 }).text
+
+# using bs4 to parse reponses
 
 soup = BeautifulSoup(r, 'html.parser')
 i = 0
