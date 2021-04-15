b0ad17b135b5750a4afd2d779ac13a24f89f240e
Sahil Shaikh <sahil.universe@gmail.com>
2021-04-15 02:00:30 -0400

    added camera, confirmation screen, loading screen

--- main.py
+++ main.py
@@ -4,25 +4,55 @@ from kivymd.uix.label import MDLabel
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen,ScreenManager
 from screen_nav import screen_helper
+from kivy.uix.camera import Camera
+from kivy.clock import Clock
+from kivymd.uix.progressbar import ProgressBar
+from kivy.cache import Cache
+import time
+import os
+
 
 from kivy.core.window import Window
 Window.size = (360, 640)
+
 
 class WelcomeScreen(Screen):
     pass
 
 class HomeScreen(Screen):
+    def capture(self):
+        camera = self.ids['camera']
+        #timestr = time.strftime("%Y%m%d_%H%M%S")
+        #camera.export_to_png("IMG_{}.png".format(timestr))
+        camera.export_to_png("IMG.png")
+        Clock.schedule_once(self.next_screen)
+
+    def next_screen(self,dt):
+        self.manager.current = 'transition'       
+
+class TransitionScreen(Screen):
+    def on_enter(self):
+        Cache.remove('image')
+        self.ids.image.source = 'IMG.png'
+
+class LoadingScreen(Screen):
+    def next_screen(self):
+        self.manager.current = 'result'
+
+class ResultScreen(Screen):
     pass
 
 sm = ScreenManager()
 sm.add_widget(WelcomeScreen(name='welcome'))
 sm.add_widget(HomeScreen(name='home'))
+sm.add_widget(TransitionScreen(name='transition'))
+sm.add_widget(TransitionScreen(name='loading'))
+sm.add_widget(TransitionScreen(name='result'))
 
 class Main(MDApp):
     def build(self):
         screen = Builder.load_string(screen_helper)
-        
-        #wimage = Image(source="media/wallpaper.jpg")
         return screen
+    
 
 Main().run()
--- screen_nav.py
+++ screen_nav.py
@@ -1,29 +1,75 @@
 screen_helper = """
-
 ScreenManager:
     WelcomeScreen:
     HomeScreen:
+    TransitionScreen:
+    LoadingScreen:
+    ResultScreen:
+
 
 <WelcomeScreen>:
     name: 'welcome'
     Image:
         source: 'media/wallpaper2.jpg'
-    MDFloatingActionButton:
-        icon: "location-enter"
+    MDRaisedButton:
+        text: "Get Started"
+        pos_hint: {"center_x": 0.5, "center_y":0.3}
+        size_hint: (0.5,0.1)
+        font_size: '20sp'
+        elevation_normal: 10
         on_press: root.manager.current = 'home'
-        halign: "center"
 
 <HomeScreen>:
     name: 'home'
-    Image:
-        source: 'media/wallpaper3.jpg'
-    MDLabel:
-        text: 'you are home'
-        pos_hint: {"center_x": 0.5, "center_y": 0.5}
-        color: 'white'
+    Camera:
+        id: camera
+        resolution: (160, 200)
     MDFloatingActionButton:
-        icon: "android"
-        on_press: root.manager.current = 'welcome'
-        halign: "center"
+        icon: "camera"
+        pos_hint: {"center_x": 0.5, "center_y":0.2}
+        shadow_pos: 0, 0
+        elevation_normal: 8
+        on_press: root.capture()
+
+<TransitionScreen>:
+    name: 'transition'
+    Image:
+        id: image
+        pos_hint: {'center_x':0.5,'center_y':0.6}
+    MDProgressBar:
+        id: progress
+        pos_hint: {'center_x': 0.5, 'center_y': 0.25}
+        size_hint_x: 0.5
+        type: "indeterminate"
+    MDRaisedButton:
+        text: "Let's Decrypt"
+        icon: "arrow_back"
+        pos_hint: {"center_x": 0.5, "center_y":0.15}
+        size_hint: (0.5,0.1)
+        font_size: '20sp'
+        elevation_normal: 10
+        on_press: root.manager.current = 'loading'
+
+<LoadingScreen>:
+    name: "loading"
+    MDSpinner:
+        size_hint: (None, None)
+        size: (dp(15), dp(15))
+        palette: [[0.28627450980392155, 0.8431372549019608, 0.596078431372549, 1], [0.3568627450980392, 0.3215686274509804, 0.8666666666666667, 1], [0.8862745098039215, 0.36470588235294116, 0.592156862745098, 1], [0.8784313725490196, 0.9058823529411765, 0.40784313725490196, 1]]
+    MDRaisedButton:
+        text: "Let's Decrypt"
+        icon: "done_all"
+        pos_hint: {"center_x": 0.5, "center_y":0.15}
+        size_hint: (0.5,0.1)
+        font_size: '20sp'
+        elevation_normal: 10
+        on_press: root.manager.current = 'result'
+
+
+<ResultScreen>:
+    name: "result"
+    Label:
+        text: "Geronimo!"
+
 
 """
